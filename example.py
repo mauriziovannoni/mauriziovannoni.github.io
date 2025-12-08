2fbd198c44a968db235cb03de27ab7d80a8caa31
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-08 15:53:20 +0100

    Update example.py

--- example.py
+++ example.py
@@ -5,7 +5,7 @@ import pandas as pd
 st.title('Useful formulas for Xray optics')
 
 st.subheader('Number of pickups by hour')
-
+st.write("Elliptical optics to focus a source point to an image point using paraxial formulas") 
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
