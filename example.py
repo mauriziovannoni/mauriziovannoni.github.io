16a13f40254649c2a670377a697a188234600b8f
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-08 16:38:26 +0100

    Update example.py

--- example.py
+++ example.py
@@ -9,10 +9,10 @@ st.write("Typical case: elliptical optics to focus a source point to an image po
 st.text_input("insert p value (meters)", value="100")
 st.text_input("insert q value (meters)", value="10")
 st.text_input("insert theta value (milliradians)", value="9")
-theta=theta*1E-3
-r=p
-rp=q
-u=np.cos(theta)*(1/r-1/rp)
+thetag=float(theta)*1E-3
+r=double(p)
+rp=double(q)
+u=np.cos(thetag)*(1/r-1/rp)
 v=1/(r*rp)
 
 
