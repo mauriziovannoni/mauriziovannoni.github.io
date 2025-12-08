2cedce19b47f54d2988af7b637e52f754fe91546
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-08 16:08:38 +0100

    Update example.py

--- example.py
+++ example.py
@@ -7,6 +7,8 @@ st.title('Useful formulas for Xray optics')
 st.subheader('Elliptical focusing mirrors')
 st.write("Typical case: elliptical optics to focus a source point to an image point using paraxial formulas") 
 st.text_input("insert p value (meters)", value="100")
+st.text_input("insert q value (meters)", value="10")
+st.text_input("insert $\theta$ value (milliradians)", value="9")
 
 
 chart_data = pd.DataFrame(
