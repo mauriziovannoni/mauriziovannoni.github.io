dafcbe1f8c2cbce52715dfc93be7f0d8b5d5be9e
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-08 15:50:37 +0100

    Update example.py

--- example.py
+++ example.py
@@ -2,6 +2,11 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
+st.title('Useful formulas for Xray optics')
+
+st.subheader('Number of pickups by hour')
+
+Print("
 chart_data = pd.DataFrame(
      np.random.randn(20, 3),
      columns=['a', 'b', 'c'])
