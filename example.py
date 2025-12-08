11ccaaaf84898cb26d2046ae717167913cf52b52
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-08 16:39:36 +0100

    Update example.py

--- example.py
+++ example.py
@@ -6,9 +6,9 @@ st.title('Useful formulas for Xray optics')
 
 st.subheader('Elliptical focusing mirrors')
 st.write("Typical case: elliptical optics to focus a source point to an image point using paraxial formulas") 
-st.text_input("insert p value (meters)", value="100")
-st.text_input("insert q value (meters)", value="10")
-st.text_input("insert theta value (milliradians)", value="9")
+p=st.text_input("insert p value (meters)", value="100")
+q=st.text_input("insert q value (meters)", value="10")
+theta=st.text_input("insert theta value (milliradians)", value="9")
 thetag=float(theta)*1E-3
 r=double(p)
 rp=double(q)
