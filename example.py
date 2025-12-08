a05b66192eed3f9aef3d3af332f17e4484fcf5be
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-08 15:42:09 +0100

    Update example.py

--- example.py
+++ example.py
@@ -1,2 +1,11 @@
-import numpy as np
-print("Hello world")
+"""
+# My first app
+Here's our first attempt at using data to create a table:
+"""
+
+import streamlit as st
+import pandas as pd
+df = pd.DataFrame({
+  'first column': [1, 2, 3, 4],
+  'second column': [10, 20, 30, 40]
+})
