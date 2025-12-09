9283613ee8050934d25e7b745e82bb0fdbe8f686
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-09 17:54:03 +0100

    Update 1_EllipticalMirror.py

--- pages/1_EllipticalMirror.py
+++ pages/1_EllipticalMirror.py
@@ -24,10 +24,15 @@ a4=a2*(5*u**2/16+v/4)
 
 x=np.arange(-0.5,0.5,0.001)
 y=a2*x**2+a3*x**3+a4*x**4
+coeff=np.polyfit(x,y,2)
+prof_bestParab=np.polyval(x,coeff)
+prof_bestParabRem=y-prof_bestParab
 #ypp=2*a2+3*2*a3*x+4*3*a4*x**2
 #r=1/max(ypp)
 df = pd.DataFrame(y*1E6)
-st.line_chart(df,x_label="mm",y_label="microns")
+st.line_chart(df,x_label="mm",y_label="microns",title="aa")
+df = pd.DataFrame(profbestParab*1E6)
+st.line_chart(df,x_label="mm",y_label="microns",title="bb")
 
 st.write("Radius of curvature at middle point (Km): ",round(1/(2*a2)*1E-3,3)) 
 #st.write(r)
