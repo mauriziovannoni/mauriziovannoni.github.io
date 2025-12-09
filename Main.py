0e84eb48c39697433e238646a16faf37d0efb3a4
mauriziovannoni <102020508+mauriziovannoni@users.noreply.github.com>
2025-12-09 15:19:07 +0100

    Create Main.py

---
+++ Main.py
@@ -0,0 +1,18 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Maurizio Vannoni Xray Optics Utility App ",
+    page_icon="",
+)
+
+st.write("# Welcome to my utility page! 👋")
+
+st.sidebar.success("Select a topic above.")
+
+st.markdown(
+    """
+    This page is collecting may useful codes for Xray optics daily business, and it is open to everyone.
+    If you find it great, I am happy! If you would like to see more tools here, you should know where to find me ;-)    
+    **👈 Select a topic from the sidebar and have fun**
+"""
+)
